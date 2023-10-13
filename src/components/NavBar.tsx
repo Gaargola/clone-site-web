4c116c55130e1f141c2707a19e3ccbffedacad29
Gaargola <tl3pr0yt@gmail.com>
2023-10-13 08:40:47 -0300

    styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,31 +4,31 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav className="flex justify-around">
-      <ul className="flex mt-10 ">
+      <ul className="flex mt-10 mr-[5%] ">
         <li className="">
-          <Link href="#" className="">Find talent</Link>
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Find talent</Link>
         </li>
-        <li className="ml-4">
-          <Link href="#">Inspiration</Link>
+        <li className="ml-8">
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Inspiration</Link>
         </li>
-        <li className="ml-4">
-          <Link href="#">Learn design</Link>
+        <li className="ml-8">
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Learn design</Link>
         </li>
-        <li className="ml-4">
-          <Link href="#">Jobs</Link>
+        <li className="ml-8">
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Jobs</Link>
         </li>
-        <li className="ml-4">
-          <Link href="#">Go Pro</Link>
+        <li className="ml-8">
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Go Pro</Link>
         </li>
       </ul>
-        <Image src="/logo.png" alt="logo" className="ml-[10rem] mr-[10rem]" layout="fixed" height={300} width={100}/>
-      <ul className="flex mt-10">
+        <Image src="/logo.png" alt="logo" className="max-w-auto cursor-pointer ease-in duration-100 hover:invert-[40%] " layout="fixed" height={300} width={100}/>
+      <ul className="flex mt-10 ml-[25%]">
         <li className="mr-4">
-          <Link href="#">Log in</Link>
+          <Link href="#" className="font-sans font-medium hover:text-[#565564]">Log in</Link>
         </li>
         <span className="mr-4">•</span>
         <li >
-          <Link href="#" className="px-7 py-4 rounded-full bg-[#0D0C22] text-white font-semibold">Sign up</Link>
+          <Link href="#" className="px-6 py-3 text-center items-center rounded-full font-sans bg-[#0D0C22] delay-100 ease-in duration-100 hover:bg-[#565564] text-white font-semibold">Sign up</Link>
         </li>
       </ul>
     </nav>
